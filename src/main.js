f81bb3e7b6a40e50ce239d1285b9fd61e72fb09b
CorentinGautier <cortech12@gmail.com>
2020-01-20 13:35:36 +0100

    ajout de css

--- src/main.js
+++ src/main.js
@@ -3,7 +3,23 @@ import App from './App.vue'
 import router from './router'
 
 
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+import Toast from 'primevue/toast';
+import ToastService from 'primevue/toastservice';
+
+Vue.use(ToastService);
+
+Vue.component('InputText', InputText);
+Vue.component('Button', Button);
+Vue.component('Toast', Toast);
+
 Vue.config.productionTip = false
+
+import 'primevue/resources/themes/nova-light/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
 
 new Vue({
     router,
